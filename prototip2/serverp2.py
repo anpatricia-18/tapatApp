a03ff760ec0f6289c2a26c4a14d9bda04f73f9b4
anpatricia-18 <ap.aguilera06@gmail.com>
2025-03-04 23:35:42 +0100

    serverp2

--- prototip2/serverp2.py
+++ prototip2/serverp2.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# Datos de ejemplo
+# Dade d'exemple
 children = [
     {"id": 1, "name": "Child 1"},
     {"id": 2, "name": "Child 2"}
